993843807160da783d36614161cee0829a0cef9e
Mohammad Ismail Hossain <118668314+CodeWithIsmail@users.noreply.github.com>
2023-05-27 10:32:24 +0600

    Add files via upload

---
+++ StaticCalculator/src/Calculator.java
@@ -0,0 +1,10 @@
+public class Calculator {
+    public static int sum(int a,int b)
+    {
+        return a+b;
+    }
+    public static double sum(double a,double b)
+    {
+        return a+b;
+    }
+}
---
+++ StaticCalculator/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Calculator.sum(2,6));
+        System.out.println(Calculator.sum(2.6,4.2));
+    }
+}
